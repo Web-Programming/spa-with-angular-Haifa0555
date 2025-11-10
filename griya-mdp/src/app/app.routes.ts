f9523d964bd6398297b5f8e5942cfa155747a5af
Haifa0555 <haifanabila_2327240124@mhs.mdp.ac.id>
2025-11-10 17:05:58 +0700

    angular

---
+++ griya-mdp/src/app/app.routes.ts
@@ -0,0 +1,37 @@
+import { Routes } from '@angular/router';
+import {Home as HomeComponent} from './home/home';
+import { Component } from '@angular/core';
+import { Profil } from './profil/profil';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import { Contact } from './contact/contact';
+
+export const routes: Routes = [
+    // mengatur halaman utama aplikasi
+  {
+    path : "",
+    component : HomeComponent,
+    //title : "Home Page"
+  },
+   {
+    path : "profile",
+    component : Profil,
+    //title : " Profil Page"
+  },
+   {
+    path : "login",
+    component : Login,
+    
+  },
+   {
+    path : "register",
+    component : Register,
+   
+  },
+   {
+    path : "contact",
+    component : Contact,
+    
+  },
+
+];
