3948148be806e9503105004934d0d42117e87095
Haifa0555 <haifanabila_2327240124@mhs.mdp.ac.id>
2025-11-10 15:55:40 +0700

    angular

---
+++ my-cv-app/src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { Cv as CvComponent } from './cv/cv';
+import { Contact as ContactComponent } from './contact/contact';
+
+export const routes: Routes = [
+ // 1. Path untuk Halaman CV
+ {
+   path: 'cv',
+   component: CvComponent,
+   title: 'Curriculum Vitae Saya'
+ },
+ // 2. Path untuk Halaman Kontak
+ {
+   path: 'contact',
+   component: ContactComponent,
+   title: 'Kontak Saya'
+ },
+ {
+   path: '',
+   redirectTo: '/cv',
+   pathMatch: 'full'
+ },
+ // 4. (Opsional) Halaman 404
+ // { path: '**', component: NotFoundComponent }
+];
